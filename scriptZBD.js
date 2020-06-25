e48b686c74b8b304fa2785a6738e11dc7649ed05
Vinny V <VinAVarghese@gmail.com>
2020-06-25 14:34:42 -0700

    responsive features added

--- scriptZBD.js
+++ scriptZBD.js
@@ -20,7 +20,7 @@ $("#Btn").on("click", function() {
         newArticle.append(newDiv);
         var newImg = $("<img>");
         newImg.attr("src", response.drinks[i].strDrinkThumb);
-        newImg.attr("class", "drinkPic")
+        newImg.attr("id", "product")
         newDiv.append(newImg);
         var divTitle = $("<div>");
         divTitle.attr("class", `article-row-content ${[i]}`);
